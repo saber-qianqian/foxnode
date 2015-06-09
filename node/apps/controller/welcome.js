301180f75004fa969e33ebe3cb2827f48430a9db
yunqian <yunqian@meilishuo.com>
2015-06-09 16:22:42 +0800

    add new controller

--- node/apps/controller/welcome.js
+++ node/apps/controller/welcome.js
@@ -6,5 +6,15 @@ function index(res, req) {
 	data._CSSLinks = ['home', 'pages/main'];
 	this.render(res, req, 'welcome.html', data);
 }
+function h(res, req) {
+	console.log(this)
+	console.log("Request handler 'welcome' was called.");
+	var data = {};
+	data.hello = "hello";
+	data.pageTitle = '<script>alert(1)</script>';
+	data._CSSLinks = ['home', 'pages/main'];
+	this.render(res, req, 'welcome.html', data);
+}
 
 exports.index = index;
+exports.h = h;
